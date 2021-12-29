2978ed7d088a88ff94384ba713275322b40bf2a7
Yehonatan <yonivid@gmail.com>
2021-12-29 15:26:23 +0200

    all stages divided

--- server/controller/trade-controller.js
+++ server/controller/trade-controller.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const data = require("../source.json");
 let source = "sourceFile";
 let unmatchedArray = [];
+const nameList = ["source", "unmatched_NVsN", "unmatched_NVs1"];
 
 router.get("/sourcetojson", async (req, res, next) => {
   await CSVToJSON()
@@ -28,7 +29,6 @@ router.get("/sourcetojson", async (req, res, next) => {
 
 router.get("/createtable", async (req, res, next) => {
   const categories = Object.keys(data[0]);
-  console.log(categories);
   try {
     const tableCreated = await tradeLogic.createTable(categories, source);
     res.json(tableCreated);
@@ -36,10 +36,11 @@ router.get("/createtable", async (req, res, next) => {
     return err;
   }
 });
+
 router.get("/pushjson", async (req, res, next) => {
   try {
-    const categories = (Object.keys(data[0]));
-    const pushJson = await tradeLogic.pushJson(categories,data, source);
+    const categories = Object.keys(data[0]);
+    const pushJson = await tradeLogic.pushJson(categories, data, source);
     res.json("push was completed");
   } catch (err) {
     return err;
@@ -49,32 +50,40 @@ router.get("/pushjson", async (req, res, next) => {
 router.get("/nvsn", async (req, res, next) => {
   try {
     const resultDash = await tradeLogic.getNVsN(source);
-
     resultDash.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
-    // console.log(unmatchedArray);
-
     res.json(resultDash);
   } catch (err) {
     return err;
   }
 });
-
 router.get("/nvs1", async (req, res, next) => {
   source = "unmatched_NVsN";
   const categories = Object.keys(unmatchedArray[0]);
   try {
-    const tableCreated = await tradeLogic.createTable(categories,source);
+    const tableCreated = await tradeLogic.createTable(categories, source);
+    // const pushJson = await tradeLogic.pushJson(categories, unmatchedArray, source);
+    // const resultDash = await tradeLogic.getNVs1(source);
+    // resultDash.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
+    // res.json(resultDash);
+    // res.json(tableCreated)
   } catch (err) {
     throw err;
   }
   try {
-    const pushJson = await tradeLogic.pushJson(categories, unmatchedArray, source);
-    res.json("push was completed");
+    const pushJson = await tradeLogic.pushJson(
+      categories,
+      unmatchedArray,
+      source,
+    );
+    // res.json("push was completed");
   } catch (err) {
     throw err;
   }
-  const resultNvs1=await tradeLogic.getNVs1(source)
+  const resultNvs1 = await tradeLogic.getNVs1(source);
+  unmatchedArray = [];
+  resultNvs1.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
 
+  res.json(resultNvs1);
   //   const data = JSON.parse(map1.get("array"))
   //   try{
   //
@@ -83,6 +92,119 @@ router.get("/nvs1", async (req, res, next) => {
   //   }
   //   catch(err){throw err}
 });
+
+router.get("/1vs1", async (req, res, next) => {
+  source = "unmatched_nVs1";
+  const categories = Object.keys(unmatchedArray[0]);
+  try {
+    const tableCreated = await tradeLogic.createTable(categories, source);
+  } catch (err) {
+    throw err;
+  }
+  try {
+    const pushJson = await tradeLogic.pushJson(
+      categories,
+      unmatchedArray,
+      source,
+    );
+  } catch (err) {
+    throw err;
+  }
+  const result1vs1 = await tradeLogic.get1Vs1(source);
+  unmatchedArray = [];
+  result1vs1.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
+
+  res.json(result1vs1);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get("/getstats", async (req, res) => {
+  //   await CSVToJSON()
+  //     .fromFile("../../seconed project/sql/baml_202101.csv")
+  //     .then((result) => {
+  //       fs.writeFile(
+  //         `${nameList[0]}.json`,
+  //         JSON.stringify(result, null, 4),
+  //         (err) => {
+  //           if (err) {
+  //             throw err;
+  //           }
+  //           console.log("JSON array is saved.");
+  //         },
+  //       );
+
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+
+  const categories = Object.keys(data[0]);
+
+  //   nameList.forEach(async (name, index) => {
+  //     console.log(index);
+  let sourceName = nameList[0];
+  try {
+    await tradeLogic.createTable(categories, sourceName);
+    await tradeLogic.pushJson(categories, data, sourceName);
+    const result1 = await tradeLogic.getNVsN(sourceName);
+    result1.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
+    ////////////////////////////////
+    sourceName = nameList[1];
+    await tradeLogic.createTable(categories, sourceName);
+    await tradeLogic.pushJson(categories, unmatchedArray, sourceName);
+    // const result2 = await tradeLogic.getNVs1(sourceName);
+    // unmatchedArray = [];
+    // result2.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
+    /////////////////
+    // sourceName = nameList[2];
+    // await tradeLogic.createTable(categories, sourceName);
+    // await tradeLogic.pushJson(categories, unmatchedArray, sourceName);
+    // const result3 = await tradeLogic.get1Vs1(sourceName);
+    // unmatchedArray = [];
+    // result2.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
+    // res.json(result2);
+  } catch (err) {
+    return err;
+  }
+  try {
+  } catch (err) {
+    return err;
+  }
+  try {
+    //     let query
+    //   switch (index) {
+    //     case 0:
+    //       query = await tradeLogic.getNVsN(sourceName);
+    //       break;
+    //     case 1:
+    //       query = await tradeLogic.getNVs1(sourceName);
+    //     case 2:
+    //       query = await tradeLogic.get1Vs1(sourceName);
+    //       break;
+    //     default:
+    //   }
+    //   query.leftTradesToFind.map((trade) => unmatchedArray.push(trade));
+    //   console.log(query.leftTradesToFind.length)
+  } catch (err) {
+    return err;
+  }
+});
+// });
 
 // router.get("/jsontosql", async (req, res, next) => {
 //   let total = 0,
@@ -116,14 +238,5 @@ router.get("/nvs1", async (req, res, next) => {
 //     throw err;
 //   }
 // });
-
-router.get("/1vs1", async (req, res, next) => {
-  try {
-    const resultDash = await tradeLogic.get1vs1();
-    res.json(resultDash);
-  } catch (error) {
-    return next(error);
-  }
-});
 
 module.exports = router;
